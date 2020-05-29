372ef07b58ef5a2602b6f530b801bc1939fcd9de
Fajar AM <somabangsa@gmail.com>
2020-05-29 23:35:07 +0700

    update filter log

--- src/main/java/com/fajar/shoppingmart/config/CustomFilter.java
+++ src/main/java/com/fajar/shoppingmart/config/CustomFilter.java
@@ -1,7 +1,10 @@
 package com.fajar.shoppingmart.config;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Date;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -32,31 +35,61 @@ public class CustomFilter implements javax.servlet.Filter {
       FilterChain chain) throws IOException, ServletException {
   
     	startTime = new Date();
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request; 
         
         boolean isRestEndpoint = true;//req.getMethod().toLowerCase().equals("post");
         
         if(isRestEndpoint) {
-	        System.out.println("****************************** BEGIN API ***************************");
-	        System.out.println("Content Type: "+ req.getContentType());
-	        System.out.println("Method: "+req.getMethod()+" \nURI: "+ req.getRequestURI());
+	        System.out.println("****************************** BEGIN API ***************************"); 
+	        System.out.println(uriInfo(req));
+	        printRequestHeaders(req);
+	        System.out.println(contentTypeInfo(req));
 	        System.out.println("********************************************************************");
 	        System.out.println();
 	         
         } 
         	
         chain.doFilter(request, response);
+        HttpServletResponse res = (HttpServletResponse) response;
+        
         endTime = new Date();
         
         if(isRestEndpoint) {
         	System.out.println();
-        	System.out.println("********************************************************************");
-        	System.out.println("URI: "+req.getRequestURI());
+        	System.out.println("***************************** END API *******************************");
+        	System.out.println(uriInfo(req));
+        	printResponseHeaders(res);
 	        System.out.println("Status: "+ res.getStatus()+" Duration: "+ getDuration()+" ms");
-	        System.out.println("Content Type: "+ res.getContentType());
-	        System.out.println("***************************** END API ******************************");
+	        System.out.println(contentTypeInfo(req));
+	        System.out.println("*********************************************************************");
         }
+    }
+    
+    private void printResponseHeaders(HttpServletResponse res) {
+    	Collection<String> headers = res.getHeaderNames(); 
+		for (String header : headers) {
+			System.out.println(header+": "+res.getHeader(header));
+		}
+		
+	}
+
+	private void printRequestHeaders(HttpServletRequest req) {
+		Enumeration<String> headers = req.getHeaderNames(); 
+		while(headers.hasMoreElements()) {
+			String header = headers.nextElement();
+			System.out.println(header+": "+req.getHeader(header));
+		}
+		 
+	}
+
+	private String contentTypeInfo(HttpServletRequest req) {
+
+    	String contentInfo = "Content Type: "+ req.getContentType();
+		return contentInfo;
+	}
+
+	private String uriInfo(HttpServletRequest req) {
+    	return("URI: ["+req.getMethod()+"]"+req.getRequestURI());
     }
 
     private long getDuration() {
