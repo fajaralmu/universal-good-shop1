300b7baa35975d94b7d72bd3e9a2c7e266217a7b
Fajar AM <somabangsa@gmail.com>
2020-05-29 20:30:11 +0700

    add json ignore in interface

--- src/main/java/com/fajar/shoppingmart/entity/custom/FinancialEntity.java
+++ src/main/java/com/fajar/shoppingmart/entity/custom/FinancialEntity.java
@@ -2,11 +2,16 @@ package com.fajar.shoppingmart.entity.custom;
 
 import java.util.Date;
 
-public interface FinancialEntity {
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+public interface FinancialEntity {
+	@JsonIgnore
 	public Date getTransactionDate();
+	@JsonIgnore
 	public String getTransactionName();
+	@JsonIgnore
 	public long getTransactionNominal();
+	@JsonIgnore
 	public BalanceJournalInfo getBalanceJournalInfo();
 	public Long getId();
 }
